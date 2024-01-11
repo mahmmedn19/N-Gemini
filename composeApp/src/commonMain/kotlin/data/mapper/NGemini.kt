e87f68ecb5235d6d428462567e96fcc00153738b
Mohamed Naser <mahmmedn19@gmail.com>
2024-01-11 23:59:02 +0200

    refactor model and add docs

--- composeApp/src/commonMain/kotlin/data/mapper/NGemini.kt
+++ composeApp/src/commonMain/kotlin/data/mapper/NGemini.kt
@@ -8,7 +8,6 @@ fun NGeminiResponseDto.toNGemini(): NGemini {
 
     return NGemini(
         candidates?.map { it.toCandidate() } ?: emptyList(),
-        promptFeedback?.toPromptFeedback() ?: NGemini.PromptFeedback(emptyList())
     )
 }
 
@@ -16,8 +15,6 @@ fun CandidateDto.toCandidate(): NGemini.Candidate {
     return NGemini.Candidate(
         content?.toContent(),
         finishReason,
-        index ?: 0,
-        safetyRatings?.map { it.toSafetyRating() } ?: emptyList()
     )
 }
 
@@ -31,18 +28,5 @@ fun ContentDto.toContent(): NGemini.Content {
 fun PartDto.toPart(): NGemini.Part {
     return NGemini.Part(
         text.orEmpty()
-    )
-}
-
-fun PromptFeedbackDto.toPromptFeedback(): NGemini.PromptFeedback {
-    return NGemini.PromptFeedback(
-        safetyRatings?.map { it.toSafetyRating() } ?: emptyList()
-    )
-}
-
-fun SafetyRatingDto.toSafetyRating(): NGemini.SafetyRating {
-    return NGemini.SafetyRating(
-        category.orEmpty(),
-        probability.orEmpty()
     )
 }
--- composeApp/src/commonMain/kotlin/domain/repository/NGeminiRepository.kt
+++ composeApp/src/commonMain/kotlin/domain/repository/NGeminiRepository.kt
@@ -6,6 +6,5 @@ import domain.models.NGemini
 interface NGeminiRepository {
     suspend fun generateContent(content: String): NGemini
 
-
     suspend fun generateContentWithImage(content: String, images: List<ByteArray> = emptyList()): NGemini
 }
